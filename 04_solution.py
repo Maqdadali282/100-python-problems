5279e3faca7dc0c6ad46227d40357618a26f82f2
Maqdad Ali <your-email@example.com>
2026-02-08 23:01:23 +0500

    Write a program that will give you the sum of 3 digits

---
+++ 04_solution.py
@@ -0,0 +1,9 @@
+def three_digit_sum():
+    num1 = eval(input("Enter first number = "))
+    num2 = eval(input("Enter 2nd number = "))
+    num3 = eval(input("Enter 3rd number = "))
+
+    sum = num1 + num2 + num3
+    print("the sum of three digits are : ",sum)
+
+three_digit_sum()
