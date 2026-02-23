262be919e4455f4270e7492342cdfed45d63dd3d
Maqdad Ali <your-email@example.com>
2026-02-23 14:42:25 +0500

    to find first 25 odd numbers

---
+++ 24_solution.py
@@ -0,0 +1,10 @@
+# Write a program to print the first 25 odd numbers
+
+
+count = 1
+num = 1
+while count < 25:
+    print(num)
+    num = num + 2
+    count = count + 1
+print("total count : ", count)
